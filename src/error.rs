3d4f784219f49c749dc36024af448cef66f0b85e
Jesús Trinidad Díaz Ramírez <jesdazrez@gmail.com>
2019-11-18 00:49:21 -0600

    Fix bare_trait_objects warning

--- src/error.rs
+++ src/error.rs
@@ -107,7 +107,7 @@ impl error::Error for Error {
         self.msg()
     }
 
-    fn cause(&self) -> Option<&error::Error> {
+    fn cause(&self) -> Option<&dyn error::Error> {
         None
     }
 }
